4f30e6bab0f70f197994c2c31277b4cbff1988e7
Claire McEnery <brian.m.small@gmail.com>
2015-07-28 14:47:26 -0500

    create restful interaction

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,13 +3,9 @@ var myApp = angular.module('myApp', []);
 myApp.controller('appCtrl', ['$scope', '$http', function($scope, $http){
 	console.log('hello world from controller');
 
-	todo1 = {
-		task : "Go to grocery store"
-	};
-	todo2 = {
-		task : "Get haircut"
-	};
+  $http.get('/todoList').success(function(res){
+    console.log(res);
+    $scope.tasks = res;
+  });
 
-	var tasks = [todo1, todo2];
-  $scope.tasks = tasks;
 }])
--- server.js
+++ server.js
@@ -3,5 +3,19 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/todoList', function(req, res){
+	console.log('I received a GET request');
+
+	todo1 = {
+		task : "Go to grocery store"
+	};
+	todo2 = {
+		task : "Get haircut"
+	};
+
+	var tasks = [todo1, todo2];
+	res.json(tasks)
+})
+
 app.listen(3000);
 console.log('server running on port 3000');
