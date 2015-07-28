40a05c13c68fe42e0c0074836755994029dbd5e7
Claire McEnery <brian.m.small@gmail.com>
2015-07-28 14:33:32 -0500

    add dummy data to controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,4 +2,14 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('appCtrl', ['$scope', '$http', function($scope, $http){
 	console.log('hello world from controller');
+
+	todo1 = {
+		task : "Go to grocery store"
+	};
+	todo2 = {
+		task : "Get haircut"
+	};
+
+	var tasks = [todo1, todo2];
+  $scope.tasks = tasks;
 }])
