5680362dde99e6c24f06b2adb801bf92457552a1
Claire McEnery <brian.m.small@gmail.com>
2015-08-27 14:14:20 -0500

    removed testing console logs

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,7 +5,6 @@ myApp.controller('appCtrl', ['$scope', '$http', function($scope, $http){
 
 var refresh = function(){
   $http.get('/todoList').success(function(res){
-    console.log(res);
     $scope.tasks = res;
     $scope.task = "";
   });
@@ -14,30 +13,24 @@ var refresh = function(){
 refresh();
 
 $scope.add = function(){
-  console.log($scope.task);
   $http.post('/todoList', $scope.task).success(function(res){
-    console.log(res);
     refresh();
   });
 }
 
 $scope.remove = function(id){
-  console.log(id + 'yep');  
-
   $http.delete('/todoList/' + id).success(function(res){
     refresh();
   });
 }
 
 $scope.edit = function(id){
-  console.log(id);
   $http.get('/todoList/' + id).success(function(res){
     $scope.task = res;
   });
 }
 
 $scope.update = function(){
-  console.log($scope.task._id);
   $http.put('/todoList/' + $scope.task._id, $scope.task).success(function(res){
     refresh();
   })
--- server.js
+++ server.js
@@ -8,16 +8,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.get('/todoList', function(req, res){
-	console.log('I received a GET request');
-    
     db.todoList.find(function(err, docs){
-    	console.log(docs);
     	res.json(docs);
     })
 })
 
 app.post('/todoList', function(req, res){
-	console.log(req.body);
 	db.todoList.insert(req.body, function(err, docs){
 		res.json(docs);
 	});
@@ -25,7 +21,6 @@ app.post('/todoList', function(req, res){
 
 app.delete('/todoList/:id', function(req, res){
   var id = req.params.id;
-  console.log('This should remove ' + id);
   db.todoList.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
   	res.json(doc);
   });
@@ -33,7 +28,6 @@ app.delete('/todoList/:id', function(req, res){
 
 app.get('/todoList/:id', function(req, res){
 	var id = req.params.id;
-	console.log('Got a get request with an ID');
 	db.todoList.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
 		res.json(doc);
 	});
@@ -41,7 +35,6 @@ app.get('/todoList/:id', function(req, res){
 
 app.put('/todoList/:id', function(req, res){
 	var id = req.params.id;
-	console.log(req.body.task);
 	db.todoList.findAndModify({query: {_id: mongojs.ObjectId(id)}, 
 		update: {$set: {task: req.body.task}},
 		new: true}, function (err, doc){
