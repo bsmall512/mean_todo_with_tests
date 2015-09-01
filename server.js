07181d375e306520af3b649f7302bd267080fa50
Claire McEnery <brian.m.small@gmail.com>
2015-09-01 12:31:21 -0500

    added comments

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ var bodyParser = require('body-parser')
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+// gets all items currently in db
 app.get('/todoList', function(req, res){
     db.todoList.find(function(err, docs){
     	res.json(docs);
     })
 })
 
+// posts new item to db
 app.post('/todoList', function(req, res){
 	db.todoList.insert(req.body, function(err, docs){
 		res.json(docs);
 	});
 })
 
+// removes item from db
 app.delete('/todoList/:id', function(req, res){
   var id = req.params.id;
   db.todoList.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
@@ -26,6 +29,7 @@ app.delete('/todoList/:id', function(req, res){
   });
 })
 
+// retrieves a single item from the db
 app.get('/todoList/:id', function(req, res){
 	var id = req.params.id;
 	db.todoList.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
@@ -33,6 +37,7 @@ app.get('/todoList/:id', function(req, res){
 	});
 })
 
+// sends updated item to the db
 app.put('/todoList/:id', function(req, res){
 	var id = req.params.id;
 	db.todoList.findAndModify({query: {_id: mongojs.ObjectId(id)}, 
